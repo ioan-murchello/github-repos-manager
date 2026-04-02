b8c42bccc8f201dc3641e766f8cb78fff330202a
Jack <jack@MacBook-Pro.local>
2026-04-02 10:09:53 +0200

    changed urls everywhere

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5001', 
+        target: 'https://github-repos-manager.onrender.com', 
       },
     }
   }
